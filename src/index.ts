43181f33fbf93da664918b0e43480e231b9b3f0e
areverdy <alec.rev1234@gmail.com>
2023-09-05 16:04:42 +0200

    ajoutAPI

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,36 @@ console.log('personne modifiee', personneModifiee.gendar)
 const p2: Ipersonne = {name : 'Laforge', surname :'Thomas', gendar : 'm', age : 31}
 console.log(p2)
 
-const fs = require('fs-extra')
+import fs from 'fs-extra'
 
-const packageObj = fs.readJsonSync('./personne.json',p2)
+const packageObj = fs.readJsonSync('./personne.json')
 console.log(packageObj.version) // => 2.0.0
+
+
+
+import express from 'express'
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+  console.log('toto');
+  res.send('Toto');
+})
+
+app.get('/date', (req, res) => {
+  const currentdate = new Date ();
+  console.log('date:', currentdate);
+  res.send(JSON.stringify({Date: currentdate}));
+})
+
+app.get('/Thomas', (req, res) => {
+  console.log('objetP2', p2)
+  res.send(JSON.stringify(p2))
+})
+
+app.get('/fichier',)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
