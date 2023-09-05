d5146343052e5c3727a2541db10f08674c9fd20f
areverdy <alec.rev1234@gmail.com>
2023-09-05 09:07:04 +0200

    mardi matin

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
 console.log('Hello world');
+console.log(process.argv);
+
+  if(process.argv.includes("--help"))  {
+      console.log("je vais vous aider") 
+    } 
+  else if (process.argv.includes("--name")) { 
+      console.log ("Bonjour " , process.argv[process.argv.indexOf('--name') + 1]) 
+  }
+    
+interface Ipersonne {
+  name : string,
+  surname : string,
+  gendar : string,
+  age : number
+}
+
+const p: Ipersonne = {name : 'Reverdy', surname :'Alexandra', gendar :'f', age : 52}
+console.log(p)
+
+const personneModifiee: Ipersonne = {
+  ...p,
+  name: "Laforge",
+  age: 26
+}
+personneModifiee.gendar = 'h'
+
+console.log('personne modifiee', personneModifiee.gendar)
+
+const p2: Ipersonne = {name : 'Laforge', surname :'Thomas', gendar : 'm', age : 31}
+console.log(p2)
